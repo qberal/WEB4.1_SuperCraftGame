0c92e08c45fb1d3ce1b25d6f0886ece90efa46fa
qberal <quentin.beral@insa-rouen.fr>
2024-11-21 13:32:53 +0100

    Test user model

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
+require('dotenv').config();
 const express = require('express');
+const User = require('./model/user');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-const port = 3000;
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('../frontend'));
-
 
 app.listen(port, () => {
     console.log(`Express app listening on port ${port}`);
+
+    User.findById(1, (err, user) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(user);
+        }
+    });
 });

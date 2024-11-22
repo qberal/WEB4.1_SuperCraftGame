4f6e16067e963786406e99bbf2b43aa4318e3d80
qberal <quentin.beral@insa-rouen.fr>
2024-11-22 19:04:26 +0100

    Add SimpleButton.vue

---
+++ frontend/src/components/SimpleButton.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+defineProps({
+  icon: String,
+  name: String,
+  onClick: Function
+});
+
+
+</script>
+
+<template>
+  <div>
+    <button @click="onClick">
+      <img :src="icon">
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+button {
+  border-radius: 10px;
+  border: 1px solid #BBB;
+  background: #ECECEC;
+  box-shadow: 0px 4px 16.3px 0px rgba(49, 49, 49, 0.15);
+  width: 50px;
+  height: 50px;
+}
+
+button:hover {
+  cursor: pointer;
+  border: 1px solid #00cdb2;
+}
+
+button:active {
+  background: #BBBBBB;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  padding: 5px;
+}
+
+
+</style>

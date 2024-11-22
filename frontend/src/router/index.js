602fc1141c5a5e69fad309faba7598bab1dd5c8f
qberal <quentin.beral@insa-rouen.fr>
2024-11-22 14:07:49 +0100

    Add router

---
+++ frontend/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import FormView from '../views/FormView.vue';
+import Login from '../components/forms/LoginForm.vue';
+import Register from '../components/forms/RegisterForm.vue';
+import ForgotPasswordForm from "@/components/forms/ForgotPasswordForm.vue";
+
+const routes = [
+    { path: '/', redirect: '/login' },
+    {
+        path: '/',
+        component: FormView,
+        children: [
+            { path: 'login', component: Login },
+            { path: 'register', component: Register },
+            { path: 'forgot-password', component: ForgotPasswordForm },
+        ],
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;

93966471b61e8a46d8ff822b40db1d4eb57cc597
qberal <quentin.beral@insa-rouen.fr>
2024-11-27 17:54:57 +0100

    Removed test from server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const User = require('./model/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,18 +13,10 @@ app.use('/fusion', fusion);
 const item = require('./routers/item');  
 app.use('/item', item);
 
-const mergeWords = require('./words.js');
+//const mergeWords = require('./words.js');
 
 app.listen(port, () => {
     console.log(`Express app listening on port ${port}`);
-
-    User.findById(1, (err, user) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(user);
-        }
-    });
 });
 
 //test route

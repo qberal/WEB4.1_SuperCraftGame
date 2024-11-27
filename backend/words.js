815eeef483158431159e6f8b64bb1e4c24e1a6c8
Mubelotix <mubelotix@gmail.com>
2024-11-27 15:31:21 +0100

    Finish mergeWords

--- backend/words.js
+++ backend/words.js
@@ -6,14 +6,29 @@ console.log("Loading model");
 w2v.loadModel('../vectors.bin', function( error, loadedModel ) {
     console.log("Model loaded");
     model = loadedModel;
-    console.log(mergeWords("king", "woman"));
 });
 
 /// Returns a word which is the sum of the two input words.
 /// Returns null if either word is not in the model.
 function mergeWords(word1, word2) {
-    let vec1 = model.getVector(word1);
-    let vec2 = model.getVector(word2);
+    let word1Lower = word1.toLowerCase();
+    let word2Lower = word2.toLowerCase();
+    let vec1 = model.getVector(word1Lower);
+    let vec2 = model.getVector(word2Lower);
+    if (vec1 == null || vec2 == null) {
+        return null;
+    }
     let result = vec1.add(vec2);
-    return model.getNearestWord(result);
+    let nearestWords = model.getNearestWords(result, 5);
+    let nearestStrings = nearestWords.map(item => item.word.toLowerCase());
+
+    for (let nearestString of nearestStrings) {
+        if (nearestString != word1Lower && nearestString != word1Lower + 's' && nearestString != word2Lower && nearestString != word2Lower + 's') {
+            console.log(`{${word1}, ${word2}} -> ${nearestString}`);
+            return nearestString;
+        }
+    }
+    return null;
 }
+
+module.exports = mergeWords;

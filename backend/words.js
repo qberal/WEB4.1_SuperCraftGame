d40e572069eda6b292de64de87bb4dea709c7362
Mubelotix <mubelotix@gmail.com>
2024-11-27 15:10:32 +0100

    Create the merge function

--- backend/words.js
+++ backend/words.js
@@ -1,7 +1,19 @@
 const w2v = require("word2vec");
 
-console.log("Loading model");
+var model;
 
-w2v.loadModel('../vectors.bin', function( error, model ) {
+console.log("Loading model");
+w2v.loadModel('../vectors.bin', function( error, loadedModel ) {
     console.log("Model loaded");
+    model = loadedModel;
+    console.log(mergeWords("king", "woman"));
 });
+
+/// Returns a word which is the sum of the two input words.
+/// Returns null if either word is not in the model.
+function mergeWords(word1, word2) {
+    let vec1 = model.getVector(word1);
+    let vec2 = model.getVector(word2);
+    let result = vec1.add(vec2);
+    return model.getNearestWord(result);
+}

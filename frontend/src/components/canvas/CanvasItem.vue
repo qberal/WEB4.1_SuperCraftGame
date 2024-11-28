e85111d9dc469c87b92d9a3a0f59667a77a0df84
qberal <quentin.beral@insa-rouen.fr>
2024-11-28 15:42:20 +0100

    Updated CanvasItem.vue with img

--- frontend/src/components/canvas/CanvasItem.vue
+++ frontend/src/components/canvas/CanvasItem.vue
@@ -6,27 +6,42 @@ defineProps({
 </script>
 
 <template>
-  <div
-      :class="['shape', data.imgSrc ? 'image-shape' : 'circle-shape']"
-      :style="{
+    <div
+        class="shape"
+        :style="{
       top: data.y + 'px',
       left: data.x + 'px',
       width: data.width + 'px',
       height: data.height + 'px',
-      backgroundImage: data.imgSrc ? `url(${data.imgSrc})` : 'none',
-      backgroundColor: data.color || 'transparent',
       pointerEvents: data.pointerEvents || 'auto',
     }"
-  >
-  </div>
+    >
+      <img v-if="data.imgSrc" :src="data.imgSrc" alt="shape" draggable="false"/>
+      <div v-else class="shape circle-shape image-shape" draggable="false">
+        <p>fusion</p>
+      </div>
+
+    </div>
+
 </template>
 
 <style scoped>
+
 .shape {
   position: absolute;
   cursor: grab;
   border-radius: 50%;
   transition: transform 0.1s;
+}
+
+img {
+  /*
+    * On veut que l'image prenne toute la place dans le conteneur
+    * et qu'elle ne soit pas déformée
+    */
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 }
 
 .shape:active {
